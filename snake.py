21860837c92c69ac5260aba527197f2235cbeab4
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2024-08-12 16:42:35 +0530

    Fix Video Display Error!!!!!!

--- snake.py
+++ snake.py
@@ -104,7 +104,7 @@ class SnakePipelineSimulator:
         plt.close()
 
     def create_video(self, motion_type, file_path):
-        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        fourcc = cv2.VideoWriter_fourcc(*'H264')
         output_path = f'{file_path}/{motion_type}.mp4'
         out = cv2.VideoWriter(output_path, fourcc, 10.0, (640, 480))
 
